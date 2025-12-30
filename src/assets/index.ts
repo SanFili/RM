59dc07b280a20f9488898b73cf1db92c540fabab
Alexandra <>
2025-12-30 22:03:26 +0300

    Select component & sort imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 export { default as Logo } from './icons/logo.svg?react';
 export { default as LightTheme } from './icons/light-theme.svg?react';
 export { default as Arrow } from './icons/arrow.svg?react';
+export { default as ArrowDropdown } from './icons/arrow-dropdown.svg?react';
 
 export { default as Loading } from './images/loading.png';
 export { default as RickAndMorty } from './images/ricknmorty.png';
--- src/pages/CharactersListPage/CharactersListPage.tsx
+++ src/pages/CharactersListPage/CharactersListPage.tsx
@@ -1,11 +1,27 @@
 import { useState } from 'react';
-import { Loader } from 'src/shared/components';
+import { Loader, Select } from 'src/shared/components';
 import { RickAndMorty } from 'src/assets';
+import cn from 'classnames';
 
 import styles from './CharactersListPage.module.scss';
 
+const species = [
+  { value: '1', label: 'Human' },
+  { value: '2', label: 'Alien' },
+  { value: '3', label: 'Humanoid' },
+  { value: '4', label: 'Animal' },
+  { value: '5', label: 'Robot' },
+];
+
+const statuses = [
+  { value: 'alive', label: 'Alive' },
+  { value: 'dead', label: 'Dead' },
+  { value: 'unknown', label: 'Unknown' },
+];
+
 const CharactersListPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [value, setValue] = useState<string | null>(null);
 
   return (
     <div className={styles.characters}>
@@ -17,7 +33,32 @@ const CharactersListPage = () => {
       {isLoading ? (
         <Loader title='Loading characters...' />
       ) : (
-        <div className={styles.characters__content}>content</div>
+        <div className={styles.characters__content}>
+          {/* Селекты добавлены для примера */}
+          <Select
+            placeholder='Species'
+            options={species}
+            onSelect={(el) => setValue(el)}
+            value={value}
+          />
+          <div style={{ marginTop: 50 }}></div>
+          <Select
+            options={statuses.map((el) => ({
+              value: el.value,
+              label: (
+                <div
+                  className={cn(styles.status, styles[`status_${el.value}`])}
+                >
+                  <p>{el.label}</p>
+                  <span />
+                </div>
+              ),
+            }))}
+            onSelect={(el) => setValue(el)}
+            value={value}
+            size='small'
+          />
+        </div>
       )}
     </div>
   );
