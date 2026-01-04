a214c52ab89dbaef3b7a90d4154f3de33bf05656
Alexandra <filimonova.alexandra@yandex.ru>
2026-01-04 22:50:41 +0300

    Revert "Revert "Создаем компонент селектора и думаем про импорты""

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,10 @@
-export { default as Logo } from './icons/logo.svg?react';
-export { default as LightTheme } from './icons/light-theme.svg?react';
+// icons
 export { default as Arrow } from './icons/arrow.svg?react';
-
+export { default as ArrowDropdown } from './icons/arrow-dropdown.svg?react';
+export { default as Close } from './icons/close.svg?react';
+export { default as LightTheme } from './icons/light-theme.svg?react';
+export { default as Logo } from './icons/logo.svg?react';
+export { default as Search } from './icons/search.svg?react';
+// images
 export { default as Loading } from './images/loading.png';
 export { default as RickAndMorty } from './images/ricknmorty.png';
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,5 @@
-export { default as Layout } from './Layout/Layout';
 export { default as Button } from './Button/Button';
+export { default as Input } from './Input/Input';
+export { default as Layout } from './Layout/Layout';
 export { default as Loader } from './Loader/Loader';
+export { default as Select } from './Select/Select';
