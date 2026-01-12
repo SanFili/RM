ea539b0ea36a0a86cc2529740e0d5e080a631660
Alexandra <>
2026-01-12 22:10:30 +0300

    add character card

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,6 +5,8 @@ export { default as Close } from './icons/close.svg?react';
 export { default as LightTheme } from './icons/light-theme.svg?react';
 export { default as Logo } from './icons/logo.svg?react';
 export { default as Search } from './icons/search.svg?react';
+export { default as Edit } from './icons/edit.svg?react';
+export { default as Check } from './icons/check.svg?react';
 // images
 export { default as Loading } from './images/loading.png';
 export { default as RickAndMorty } from './images/ricknmorty.png';
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from './Input/Input';
 export { default as Layout } from './Layout/Layout';
 export { default as Loader } from './Loader/Loader';
 export { default as Select } from './Select/Select';
+export { default as FormItem } from './FormItem/FormItem';
