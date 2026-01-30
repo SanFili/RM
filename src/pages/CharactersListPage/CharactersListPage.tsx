8af5ed25d6476f2ed45f23e7caf0a8967af35ec2
Alexandra <>
2026-01-30 23:18:32 +0300

    add infinite scroll

--- src/pages/CharactersListPage/CharactersListPage.tsx
+++ src/pages/CharactersListPage/CharactersListPage.tsx
@@ -1,48 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
+import { memo } from 'react';
 
 import { RickAndMorty } from 'src/assets';
 
 import { Loader } from 'src/shared/components';
-import { characterFiltersType, characterType } from 'src/shared/types';
-import { debounce, getData } from 'src/shared/utils';
+import { useLoadCharacters } from 'src/shared/hooks';
+import { characterType } from 'src/shared/types';
 
 import { CharacterCard, CharactersFilters } from 'src/widgets';
 
+import InfiniteScroll from '../../shared/components/InfiniteScroll/InfiniteScroll';
 import styles from './CharactersListPage.module.scss';
 
 const CharactersListPage = () => {
-  const [characters, setCharacters] = useState<characterType[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [filters, setFilters] = useState<characterFiltersType>({
-    name: '',
-    species: '',
-    gender: '',
-    status: '',
-  });
-
-  const getCharactersList = async (params, signal) => {
-    setIsLoading(true);
-
-    const res = await getData({
-      url: `/character`,
-      params,
-      signal,
-      errorMessage: 'Couldn`t load the list of characters.',
-    });
-
-    if (res.results) setCharacters(res.results);
-
-    setIsLoading(false);
-  };
-
-  const debouncedFetch = useRef(debounce(getCharactersList, 300));
-
-  useEffect(() => {
-    const controller = new AbortController();
-    debouncedFetch.current(filters, controller.signal);
-
-    return () => controller.abort();
-  }, [filters]);
+  const {
+    characters,
+    filters,
+    setFilters,
+    isLoading,
+    loadMore,
+    isLoadingMore,
+  } = useLoadCharacters();
 
   return (
     <div className={styles.characters}>
@@ -51,24 +28,38 @@ const CharactersListPage = () => {
         alt='Rick and Morty'
         className={styles.characters__logo}
       />
+
       <div className={styles.characters__content}>
         <CharactersFilters filters={filters} setFilters={setFilters} />
         {isLoading ? (
           <Loader title='Loading characters...' />
-        ) : characters.length ? (
-          <div className={styles.characters__cards}>
-            {characters.map((character) => (
-              <CharacterCard key={character.id} data={character} />
-            ))}
-          </div>
         ) : (
-          <p className={styles.characters__empty}>
-            Characters list is empty...
-          </p>
+          <>
+            {characters.length ? (
+              <CharactersList characters={characters} />
+            ) : (
+              <p className={styles.characters__empty}>
+                Characters list is empty...
+              </p>
+            )}
+            <InfiniteScroll isLoading={isLoadingMore} loadMore={loadMore} />
+          </>
         )}
       </div>
     </div>
   );
 };
+
+const CharactersList = memo(
+  ({ characters }: { characters: characterType[] }) => {
+    return (
+      <div className={styles.characters__cards}>
+        {characters.map((character) => (
+          <CharacterCard key={character.id} data={character} />
+        ))}
+      </div>
+    );
+  },
+);
 
 export default CharactersListPage;
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button/Button';
 export { default as ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
+export { default as InfiniteScroll } from './InfiniteScroll/InfiniteScroll';
 export { default as Input } from './Input/Input';
 export { default as Layout } from './Layout/Layout';
 export { default as Loader } from './Loader/Loader';
