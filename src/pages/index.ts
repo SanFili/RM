1789ede53e171913525ad301f4c42e0acdf63a33
Alexandra <>
2026-02-15 19:13:26 +0300

    add not found page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as CharacterInfoPage } from './CharacterInfoPage/CharacterInfoPage';
 export { default as CharactersListPage } from './CharactersListPage/CharactersListPage';
+export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
